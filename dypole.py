34446c9e8fc83deae09c1572eda3b5fd034495b7
Henrik Bradtmüller <67208403+hbrmn@users.noreply.github.com>
2021-08-17 15:56:26 -0300

    Completed Data Processing and SED and T1 analysis beta

--- dypole.py
+++ dypole.py
@@ -18,12 +18,13 @@ process2d
 '''
 
 import csv
-import nmrglue as ng
 import numpy as np
-import matplotlib.pyplot as plt
-from cycler import cycler
-import palettable
 import scipy.optimize as opt
+import nmrglue as ng
+import matplotlib.pyplot as plt
+import palettable
+from cycler import cycler
+
 
 class Dataset:
     ''' Docstring
@@ -54,6 +55,10 @@ class Dataset:
         plt.rc('ytick', labelsize=10)
         plt.rc('font', **{'family':'serif', 'serif':['Times New Roman']})
 
+    def reset(self):
+        del self.spec
+        del self.area
+        
     def get_data(self):
         ''' Fetches the dictionary and NMR data from Bruker binary data. 
             The data is then prepared by removing the group delay
@@ -94,64 +99,265 @@ class Dataset:
             ''' Fetches the dictionary and NMR data from VNMR (Varian) 
                 binary data.
             '''
-            self.dic, self.rawdata = ng.varian.read(self.path)
+            self.dic, rawdata = ng.varian.read(self.path)
+            self.rawdata = rawdata[0]
             self.car_freq = np.float64(
                 self.dic['procpar']['reffrq1']['values'][0])
-            self.spec_width = np.float64(self.dic['procpar']['sw']['values'][0])
+            self.spec_width = np.float64(
+                self.dic['procpar']['sw']['values'][0])
             self.rel_off_freq = (
                 np.float64(self.dic['procpar']['sfrq']['values'][0])
                 - self.car_freq)*1e6
 
     ##### Processing #####
 
-    def process_data(self, left_shift=0, zero_fill=2, cutoff=0, exp_lb=0):
+    def process(self):
+        
         '''Returns frequency and ppm scales of input dataset as well as
         the data array, data dictionary and transmitter offset value
         (SFO) in points
+        
+        Todo: Make user choose either a string of all inputs e.g. 2, 0.5, 3,
+        or to choose following the script
+        Fix trimming not yielding 2^n number
+        '''
+        def user_input(func):
+            def inner(self, data):
+                done = 0
+                while done != 1:
+                    plt.plot(data[self.index][0])
+                    plt.show()
+                    proc_data = func(self, data)
+                    done = int(input('Selection OK? 1 - yes, 2 - no: '))
+                return proc_data
+            return inner
+        
+        @user_input
+        def left_shift(self, data):
+            left_shift = int(input('Enter number of points to left shift: '))
+            if data.ndim == 1:
+                data = data[left_shift:]
+            else:
+                data = data[:, left_shift:]
+            plt.plot(data[self.index][0][0:25])
+            plt.show()
+            return data
+        
+        @user_input
+        def trim(self, data):
+            cutoff = float(input('Enter fraction (e.g. 0.5) of FID to be used: '))
+            # Calculates the number of points after which FID is set to zeros
+            trim = int(cutoff*self.rawdata.shape[self.rawdata.ndim-1])
+    
+            # if data.ndim == 1:
+            #     data[trim:] = np.zeros(len(data[trim:]))
+            # else:
+            #     data[ :, trim:] = np.zeros(len(data[0, trim:]))
+            if data.ndim == 1:
+                data = data[0:trim]
+            else:
+                data = data[:, 0:trim]
+            plt.plot(data[self.index][0])
+            plt.show()
+            return data
+        
+        @user_input          
+        def zero_fill(self, data):
+            zero_fill = int(input('Enter multiplier of number of points: '))
+            data = ng.proc_base.zf_double(data, zero_fill-1)
+            plt.plot(data[self.index][0])
+            plt.show()
+            return data
+        
+        @user_input   
+        def apodize(self, data):
+            exp_lb = int(input('Enter exponential line broadeing in Hz: '))
+            # Calculates Line broadening (Topspin style) and
+            # divides value by sw in Hz
+            data = (
+                ng.proc_base.em(data, lb=(exp_lb/(self.spec_width))))
+            plt.plot(data[self.index][0])
+            plt.show()
+            return data
+        
+        def phase(self, spec):
+            spec = ng.proc_autophase.autops(spec, "acme", p0=0, p1=0)
+            plt.plot(spec[self.index][0])
+            plt.show()
+            return spec
+        
+        # def phase(self, spec):
+        #     self.zero_order = 0
+        #     self.first_order = 0
+        #     done = 0
+        #     while done != 1:
+        #             spec = zero_phase(self, spec)
+        #             spec = first_phase(self, spec)
+        #             done = int(input('Selection OK? 1 - yes, 2 - no: '))
+        #     return spec
 
-        Todo - make interactable to reduce arguments
+        # @user_input  
+        # def zero_phase(self, spec):
+        #     self.zero_order = int(input('Enter zero order phasing: '))
+        #     spec = ng.proc_base.ps(spec, p0=self.zero_order, 
+        #                            p1=self.first_order)
+        #     plt.plot(spec[self.index][0])
+        #     plt.show()
+        #     return spec
+        
+        # @user_input
+        # def first_phase(self, spec):
+        #     self.first_order = int(input('Enter first order phasing: '))
+        #     spec = ng.proc_base.ps(spec, p0=self.zero_order, 
+        #                            p1=self.first_order)
+        #     plt.plot(spec[self.index][0])
+        #     plt.show()
+        #     return spec
+            
 
+        # def auto_phase(self, spec):
+            
+        # Begin data processing
+        try:
+            self.proc_data
+        except: 
+            proc = 1
+        else: 
+            proc = int(input('Re-process data?: 1 - yes, 2 - no: '))
+            
+        if proc == 1:
+            data = self.rawdata
+            
+            self.index = np.where(data**2 == np.max(data**2))[0]
+            plt.show()
+            
+            data = left_shift(self, data)
+            
+            data = trim(self, data)
+            
+            data = zero_fill(self, data)
+            
+            length = data.shape[data.ndim-1]
+        
+            # Create frequency axis
+            self.freq_scale = ((np.arange((self.spec_width/2)+self.rel_off_freq,
+                               (-self.spec_width/2)+self.rel_off_freq,
+                               -self.spec_width/length)))
+            
+            # Create ppm axis
+            self.ppm_scale = self.freq_scale/self.car_freq
+            
+            # Transmitter offset frequency in points
+            # Selecting data size out of array
+            self.sfo_point = (int(np.round(length/2) 
+                                  + (self.rel_off_freq 
+                                     / ((self.spec_width)/length)
+                                     )))
+        
+            data = apodize(self, data)
+            
+            self.proc_data = data  
+        
+            # Fourier transform
+            spec = ng.proc_base.fft(self.proc_data)
+            
+            # Normalize data - autophase works faster
+            spec = spec/np.max(spec)
+    
+            spec = phase(self, spec)        
+    
+            spec = ng.proc_base.di(spec)  # Discard the imaginaries
+            if self.vendor == 'bruker':
+                spec = ng.proc_base.rev(spec) # Reverse the spectrum
+            
+            # spec = [(spec[i] -  bg_corr(self.freq_scale, spec[i], 6, 0.002)) 
+            #         for i in range(self.rawdata.shape[0])]
+            
+            self.spec = spec
+
+
+    def integrate(self, back_corr=False):
+        
+        '''Returns the sum of the datapoints in the specified intervals
+        
         Parameters
         ----------
-        left_shift :
-        zero_fill :
-        cutoff :
-        exp_lb :
+        data : numpy array of float
+            Possible 2D dataset containing spectral intensities
+        
+        Returns
+        -------
+        area : numpy array of float
+            Array of integrated intensities. Array has one column for SED data
+            input and two columns for REDOR and RESPDOR, S and S0 respectively.
+        
         '''
-        data = self.rawdata()
-
-        # Calculates the number of points after which FID is set to zeros
-        trim = int(cutoff*data.shape[data.ndim-1])
-
-        if data.ndim == 1:
-            data = data[left_shift:]
-            data[trim:] = np.zeros(len(data[trim:]))
-        else:
-            data = data[:, left_shift:]
-            data[ :, trim:] = np.zeros(len(data[0, trim:]))
-
-        # Zero fill
-        data = ng.proc_base.zf_double(data, zero_fill)
-
-        # Calculates Line broadening (Topspin style) and
-        # divides value by sw in Hz
-        data = (ng.proc_base.em(data, lb=(exp_lb/(self.spec_width))))
-
-        # Create frequency axis
-        freq = ((np.arange((self.spec_width/2)+self.rel_off_freq,
-                           (-self.spec_width/2)+self.rel_off_freq,
-                           -self.spec_width/data.shape[data.ndim-1])))
-        # Create ppm axis
-        ppm = freq/self.car_freq
-
-        # Fourier transform
-        spec = ng.proc_base.fft(data)
-
-        # Normalize data - autophase works faster
-        spec = spec/np.max(spec)
-
-        return(freq, ppm, spec)
-
+        self.process()
+        
+        try:
+            self.area
+        except: 
+            integrate = 1
+        else: 
+            integrate = int(input('Re-integrate data?: 1 - yes, 2 - no: '))
+            
+        if integrate == 1:
+            
+            ppm_scale = self.ppm_scale
+            spec = self.spec
+            # ppm per point
+            ppm_per_pt = ((np.abs(ppm_scale[-1])
+                           + np.abs(ppm_scale[0])) / len(ppm_scale))
+            ylim = np.max(spec)
+            
+            plt.plot(ppm_scale, spec[self.index[0]])
+            plt.xlim(ppm_scale[int(len(ppm_scale)*0.33)], 
+                     ppm_scale[int(len(ppm_scale)*0.66)])
+            plt.ylim(-ylim*0.05, ylim*1.05)
+            plt.show()
+            
+            done = 0
+            while done != 1:
+                x_low = int(input('Enter left bound for integration in ppm: '))
+                x_high = int(input('Enter right bound for integration in ppm: '))
+                min_int = int(self.sfo_point - x_low / ppm_per_pt)
+                max_int = int(self.sfo_point - x_high / ppm_per_pt)
+                
+                plt.plot(ppm_scale, spec[self.index[0]])
+                plt.hlines(0, np.max(ppm_scale), np.min(ppm_scale))
+                plt.xlim(ppm_scale[int(min_int)-10], 
+                         ppm_scale[int(max_int)+12])
+                plt.ylim(-0.05, ylim)
+                plt.vlines(x_low, 0, ylim)
+                plt.vlines(x_high, 0, ylim)
+                plt.show()
+                done = int(input('Selection OK? 1 - yes, 2 - no: '))
+            
+    
+            # Number of points in F2 dimension
+            #numberDirectPoints = data['dictionary']['acqus']['TD']
+            # Number of points in F1 dimension
+            num_points = spec.shape[0]
+            
+            # Initialize area parameter
+            if self.experiment == 'REDOR' or self.experiment ==  'RESPDOR':
+                area = (np.ones(num_points).reshape(int(num_points/2), 2))
+            elif self.experiment == 'SED' or self.experiment ==  'T1':
+                area = np.ones(num_points)
+            
+            # Background correction and integration
+            
+            area = [np.sum(spec[i, min_int:max_int]) for i in range(num_points)
+                    if self.experiment == 'SED' or self.experiment == 'T1']
+            
+            if self.experiment == 'REDOR' or self.experiment == 'RESPDOR':
+                for i in range(num_points):
+                    # Puts REDOR (S) and Echo (S0) intensities in same 
+                    # variable but different columns
+                    area[int(np.floor(i*0.5)), i%2] = (
+                        np.sum(spec[i, min_int:max_int]))
+            self.area = area
+        
     ##### Exporting #####
     
     def export(self, xaxis, yaxis, xlab, ylab, data_type, result=None):
@@ -191,7 +397,7 @@ class Dataset:
             vdlist = (np.array(
                 self.dic['procpar']['t1Xecho']['values']).astype(float))
 
-        # time scale in ms²
+        # time scale (2tau) in ms²
         time = (((vdlist*1e-3)*2)**2)
 
         # Calculates log(I/I0)
@@ -265,6 +471,67 @@ class Dataset:
             fig.savefig(self.path + '\\' + self.name + '_SED.png',
                         format='png', dpi=300, bbox_inches='tight')
         return second_moment
+    
+    def t1_eval(self):
+        '''
+        
+
+        Returns
+        -------
+        None.
+
+        '''
+        
+        self.experiment = 'T1'
+        
+        self.integrate()
+        
+        if self.vendor == 'varian':
+            vdlist = (np.array(
+                self.dic['procpar']['d2']['values']).astype(float))
+            
+        # time scale in s
+        time = vdlist
+        
+        # normalized intensities
+        t1_int = self.area/np.max(self.area)
+        
+        # Plot data
+        plt.plot(time, t1_int)
+        plt.show()
+        
+        def fit_func(time, amp, T1, beta):
+            return amp*(1-np.exp(-(time/T1)**beta)) # saturation recovery equation
+        
+        [popt, _] = (opt.curve_fit(fit_func, time, t1_int, 
+                                   bounds=(np.array([0, 0, 0]), 
+                                           np.array([np.inf, np.inf, 1]))))
+        
+        amp, T1, beta = popt[0], popt[1], popt[2]
+        
+        # Exporting data
+
+        # Experimental data
+        self.export(time, t1_int, 'tau', 'I', 'T1-Satrec')
+        # Fit
+        scale = np.round(np.linspace(time[0], time[-1], 1000), decimals=4)
+        fit = fit_func(scale, popt[0], popt[1])
+        result = ('T1 = '+ str(np.round(T1, decimals=4)) + ' s' 
+                  + 'beta = ' + str(np.round(beta, decimals=4)))
+        self.export(scale, fit, 'tau', 'I', 'Fit', result)
+
+        # Creating and saving SED plot
+        fig = plt.figure(figsize=(self.fig_width, self.fig_height))
+
+        plt.plot(time, t1_int)
+        plt.plot(scale, fit_func(scale, popt[0], popt[1]), '--', color='b')
+        
+        plt.xlabel(r'$\tau$ / s')
+        plt.ylabel(r'I/I$_0$')
+        fig.savefig(self.path + '\\' + self.name + '_T1.png',
+                    format='png', dpi=300, bbox_inches='tight')
+        return T1
+        
 
 def bg_corr(xaxis, yaxis, order, threshold):
 
@@ -336,10 +603,12 @@ def bg_corr(xaxis, yaxis, order, threshold):
 
 #----------------------------------------------------------------------------#
 
-Path = (r'C:\Users\HB\data_work\Projects\1_Crystallization_I\7Li\LS2\SED' 
-        + r'\210722-7Li-LS2-cryst_SEDLT.fid')
-nmr_data = Dataset(Path, 'deleteMe', 'varian')
+Path = (r'C:\Users\HB\data_work\Projects\1_Crystallization_I\7Li_Satrec\60d') 
+#         # + r'\210722-7Li-LS2-cryst_SEDLT.fid')
+nmr_data = Dataset(Path, 'TEST', 'varian')
+T1 = nmr_data.t1_eval()
 
-M2 = nmr_data.sed_fid(export='full')
+# M2 = nmr_data.sed_fid(export='zoom')
+# test_var = nmr_data.t1_eval()
 
 #if __name__ == '__main__':
